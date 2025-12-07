bb599b2447b5b3b46d0933a7378670b1c5427ce8
tshimizu <f2021212010@gmail.com>
2025-12-07 18:06:29 +0900

    create_base

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tshimizu <tshimizu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 12:48:22 by tshimizu          #+#    #+#             */
-/*   Updated: 2025/12/06 16:41:23 by tshimizu         ###   ########.fr       */
+/*   Updated: 2025/12/07 13:00:13 by tshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef struct s_parse_result
 
 bool				init_signal_handlers(void);
 
+bool  				execute(t_parse_result parse_result);
 void				free_token_list(t_token_list *list);
 void				free_split(char **arr);
 
