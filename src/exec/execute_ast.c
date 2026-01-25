6cc5110bd81a6aff77c5039a48697f788b10c974
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-01-25 23:49:27 +0900

    refactor(exec): norminetteの解消

--- src/exec/execute_ast.c
+++ src/exec/execute_ast.c
@@ -6,14 +6,39 @@
 /*   By: nkojima <nkojima@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 22:42:34 by nkojima           #+#    #+#             */
-/*   Updated: 2026/01/25 23:14:41 by nkojima          ###   ########.fr       */
+/*   Updated: 2026/01/25 23:35:12 by nkojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 #include "minishell.h"
 
-extern char	**environ;
+extern char		**environ;
+
+static int	pipe_fork_fail(int fd[2], pid_t left)
+{
+	close(fd[0]);
+	close(fd[1]);
+	if (left > 0)
+		waitpid(left, NULL, 0);
+	return (EXIT_FAILURE);
+}
+
+static pid_t	fork_pipe_child(t_ast_node *node, int fd[2], int is_left)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid != SYSCALL_SUCCESS)
+		return (pid);
+	if (is_left)
+		dup2(fd[1], STDOUT_FILENO);
+	else
+		dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	exit(execute_ast(node));
+}
 
 static int	execute_cmd_node(t_ast_node *node)
 {
@@ -29,34 +54,23 @@ static int	execute_pipe_node(t_ast_node *node)
 	int		fd[2];
 	pid_t	left;
 	pid_t	right;
-	int		status_left;
 	int		status_right;
 
-	if (pipe(fd) == -1)
-		return (1);
-	left = fork();
-	if (left == 0)
-	{
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		exit(execute_ast(node->left));
-	}
-	right = fork();
-	if (right == 0)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		exit(execute_ast(node->right));
-	}
+	if (pipe(fd) == SYSCALL_ERROR)
+		return (EXIT_FAILURE);
+	left = fork_pipe_child(node->left, fd, 1);
+	if (left == SYSCALL_ERROR)
+		return (pipe_fork_fail(fd, -1));
+	right = fork_pipe_child(node->right, fd, 0);
+	if (right == SYSCALL_ERROR)
+		return (pipe_fork_fail(fd, left));
 	close(fd[0]);
 	close(fd[1]);
-	waitpid(left, &status_left, 0);
+	waitpid(left, NULL, 0);
 	waitpid(right, &status_right, 0);
 	if (WIFEXITED(status_right))
 		return (WEXITSTATUS(status_right));
-	return (1);
+	return (EXIT_FAILURE);
 }
 
 int	execute_ast(t_ast_node *node)
