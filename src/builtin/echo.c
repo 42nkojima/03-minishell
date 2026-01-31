96e7aaa1e6a58da9edaebb268dd8926adcf4f671
tshimizu <f2021212010@gmail.com>
2026-01-31 19:05:50 +0900

    fix:レビューで指摘されたオプション処理

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,12 +6,27 @@
 /*   By: tshimizu <tshimizu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 18:20:39 by tshimizu          #+#    #+#             */
-/*   Updated: 2026/01/12 18:20:45 by tshimizu         ###   ########.fr       */
+/*   Updated: 2026/01/31 19:04:08 by tshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	is_n_flag(const char *str)
+{
+	int	i;
+
+	if (!str || str[0] != '-' || str[1] == '\0')
+		return (false);
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);
+}
 int	builtin_echo(char **argv)
 {
 	int		i;
@@ -19,7 +34,7 @@ int	builtin_echo(char **argv)
 
 	i = 1;
 	newline = true;
-	if (argv[1] && !ft_strcmp(argv[1], "-n"))
+	while (argv[i] && is_n_flag(argv[i]))
 	{
 		newline = false;
 		i++;
